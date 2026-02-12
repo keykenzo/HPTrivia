2642c6f49aff79212bf2c7328445aff150cc08fb
Mario Duarte <mariods7@gmail.com>
2026-02-12 17:18:41 -0300

    setup the menu screen and animations on instructions/info button

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
     @State private var animateViewsIn = false
     @State private var audioPlayer: AVAudioPlayer!
     @State private var scalePlayButton = false
+    @State private var showInstructions = false
+    @State private var showSettings = false
+    @State private var playGame = false
     
     var body: some View {
         GeometryReader { geo in
@@ -55,30 +58,72 @@ struct ContentView: View {
 //                    Spacer()
 //                    Spacer()
                     
-                    VStack {
-                        if animateViewsIn {
-                            Button {
-                                //Play Game
-                            } label: {
-                                Text("Play")
-                                    .font(.largeTitle)
-                                    .foregroundStyle(.white)
-                                    .padding(.vertical, 7)
-                                    .padding(.horizontal, 50)
-                                    .background(.brown.mix(with: .black, by: 0.3))
-                                    .clipShape(.rect(cornerRadius: 20))
-                                    .shadow(radius: 5)
-                                    .scaleEffect(scalePlayButton ? 1.2 : 1)
-                                    .onAppear{
-                                        withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
-                                            scalePlayButton.toggle()
-                                        }
-                                    }
+                    HStack {
+                        Spacer()
+                        
+                        VStack {
+                            if animateViewsIn {
+                                Button {
+                                    showInstructions.toggle()
+                                } label: {
+                                    Image(systemName: "info.circle.fill")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .shadow(radius: 5)
+                                }
+                                .transition(.offset(x:-geo.size.width/4))
                             }
-                            .transition(.offset(y: geo.size.height/3))
                         }
+                        .animation(.easeOut(duration: 0.7).delay(2.5), value: animateViewsIn)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            if animateViewsIn {
+                                Button {
+                                    playGame.toggle()
+                                } label: {
+                                    Text("Play")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .padding(.vertical, 7)
+                                        .padding(.horizontal, 50)
+                                        .background(.brown.mix(with: .black, by: 0.3))
+                                        .clipShape(.rect(cornerRadius: 20))
+                                        .shadow(radius: 5)
+                                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                                        .onAppear{
+                                            withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                                scalePlayButton.toggle()
+                                            }
+                                        }
+                                }
+                                .transition(.offset(y: geo.size.height/3))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            if animateViewsIn {
+                                Button {
+                                    showSettings.toggle()
+                                } label: {
+                                    Image(systemName: "gearshape.fill")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .shadow(radius: 5)
+                                }
+                                .transition(.offset(x: geo.size.width/4))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2.5), value: animateViewsIn)
+                        
+                        
+                        Spacer()
                     }
-                    .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
+                    .frame(width: geo.size.width)
                     
                     Spacer()
                 }
@@ -90,6 +135,9 @@ struct ContentView: View {
         .onAppear {
             animateViewsIn = true
 //            playAudio()
+        }
+        .sheet(isPresented: $showInstructions) {
+            Instructions()
         }
     }
     
