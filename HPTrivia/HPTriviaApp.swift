33a249c9244a4d7f8c9d780a417e8a0769628822
Mario Duarte <mariods7@gmail.com>
2026-02-12 13:42:11 -0300

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Mario Duarte on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
