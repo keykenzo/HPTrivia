36afa5430ad8b3373fa3a14f7e0dedb7888ece77
Mario Duarte <mariods7@gmail.com>
2026-02-13 15:45:11 -0300

    create the book model

---
+++ HPTrivia/Models/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  HPTrivia
+//
+//  Created by Mario Duarte on 13/02/26.
+//
+
+struct Book: Identifiable {
+    let id: Int
+    let image: String
+    let questions: [Question]
+    
+    var status: BookStatus
+    
+    enum BookStatus {
+        case active, inactive, locked
+    }
+}
