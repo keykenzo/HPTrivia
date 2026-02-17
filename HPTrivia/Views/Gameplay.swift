5e47da2c2978ed85a2a5cf5bdf639795233dfc1b
Mario Duarte <mariods7@gmail.com>
2026-02-17 13:29:08 -0300

    create sound effects and dispatch queue at gameplay.swift file

--- HPTrivia/Views/Gameplay.swift
+++ HPTrivia/Views/Gameplay.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct Gameplay: View {
+    
+    @Environment(Game.self) private var game
+    @Environment(\.dismiss) private var dismiss
+    @State private var musicPlayer: AVAudioPlayer!
+    @State private var sfxPlayer: AVAudioPlayer!
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -38,9 +45,49 @@ struct Gameplay: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+            game.startGame()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                            playMusic()
+            }
+        }
     }
+    
+    private func playMusic() {
+        
+        let songs = ["let-the-mystery-unfold", "spellcraft", "hiding-place-in-the-forest", "deep-in-the-dell"]
+        
+        let song = songs.randomElement()!
+        
+        let sound = Bundle.main.path(forResource: song, ofType: "mp3")
+        musicPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        musicPlayer.numberOfLoops = -1
+        musicPlayer.volume = 0.25
+        musicPlayer.play()
+    }
+    
+    private func playFlipSound() {
+        let sound = Bundle.main.path(forResource: "page-flip", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
+    private func playWrongSound() {
+        let sound = Bundle.main.path(forResource: "negative-beeps", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
+    private func playCorrectSound() {
+        let sound = Bundle.main.path(forResource: "magic-wand", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    
 }
 
 #Preview {
     Gameplay()
+        .environment(Game())
 }
