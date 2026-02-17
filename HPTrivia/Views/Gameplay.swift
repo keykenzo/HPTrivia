120495e9455735d3957d802f64570bc0c2c4e730
Mario Duarte <mariods7@gmail.com>
2026-02-17 14:01:42 -0300

    setup rotation effect, rotation 3D effect and hints label

--- HPTrivia/Views/Gameplay.swift
+++ HPTrivia/Views/Gameplay.swift
@@ -15,6 +15,7 @@ struct Gameplay: View {
     @State private var musicPlayer: AVAudioPlayer!
     @State private var sfxPlayer: AVAudioPlayer!
     @State private var animateViewsIn = false
+    @State private var revealHint = false
     
     var body: some View {
         GeometryReader { geo in
@@ -63,8 +64,52 @@ struct Gameplay: View {
                     
                     // MARK: Hints
                     
+                    HStack {
+                        VStack {
+                            if animateViewsIn {
+                                Image(systemName: "questionmark.app.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100)
+                                    .foregroundStyle(.cyan)
+                                    .padding()
+                                    .transition(.offset(x: -geo.size.width/2))
+                                    .phaseAnimator([false, true]) { content, phase in
+                                        content
+                                            .rotationEffect(.degrees(phase ? -13: -17))
+                                    } animation: { phase in
+                                            .easeInOut(duration: 0.7)
+                                    }
+                                    .onTapGesture {
+                                        withAnimation(.easeOut(duration: 1)) {
+                                            revealHint = true
+                                        }
+                                        playFlipSound()
+                                        game.questionScore -= 1
+                                    }
+                                    .rotation3DEffect(.degrees(revealHint ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                    .scaleEffect(revealHint ? 5 : 1)
+                                    .offset(x: revealHint ? geo.size.width/2 : 0)
+                                    .opacity(revealHint ? 0 : 1)
+                                    .overlay {
+                                        Text(game.currentQuestion.hint)
+                                            .padding(.leading, 20)
+                                            .minimumScaleFactor(0.5)
+                                            .multilineTextAlignment(.center)
+                                            .opacity(revealHint ? 1 : 0)
+                                            .scaleEffect(revealHint ? 1.33 : 1)
+                                    }
+                            }
+                        }
+                        .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    
                     // MARK: Answers
                     
+                    Spacer()
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
             
